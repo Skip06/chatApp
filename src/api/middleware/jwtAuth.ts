7ea183a2d9fb28bbb9a7afd1f943fe2221bb5a73
Swastik <swastik.patro02@gmail.com>
2026-01-26 17:30:48 +0530

    auth midd n channels route

--- src/api/middleware/jwtAuth.ts
+++ src/api/middleware/jwtAuth.ts
@@ -1,2 +1,28 @@
 import jwt from 'jsonwebtoken'
+import type {Request, Response, NextFunction} from 'express'
 
+export interface AuthRequest extends Request {
+    user?: { userId: string };
+  }
+  
+  export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+
+    const token = authHeader && authHeader.split(' ')[1]; //excludes the bearer word
+  
+    if (!token) {
+      return res.status(401).json({ error: "Token missing" });
+    }
+  
+    try {
+      const secret = process.env.JWT_SECRET!;
+      const decoded = jwt.verify(token, secret) as { userId: string }; // excludes the iat 
+      
+      // Attach the user ID to the request object for use in other routes
+      req.user = decoded;    
+      next(); 
+    } catch (err) {
+      return res.status(403).json({ error: "Invalid " });
+    }
+  };
+export default authenticateToken
---
+++ src/api/routes/channels.ts
@@ -0,0 +1,18 @@
+import {Router} from 'express'
+import authenticateToken from '../middleware/jwtAuth'
+import prisma from '../../lib/prisma'
+
+const router = Router()
+
+router.post('/create',authenticateToken, async(req: any, res)=>{
+    const {name} = req.body
+    const creatorId = req.user.userId
+
+    const channel = await prisma.channel.create({
+        data: {
+            name,
+            members: { connect: { id: creatorId } } // link this channel to existing user whose id is creatorId
+          }
+    })
+    res.json(channel)
+})
